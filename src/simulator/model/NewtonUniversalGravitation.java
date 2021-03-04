0a4ee2b605b1f5e21f246cbde9e09e406f33ac84
crisselene <criisalways@gmail.com>
2021-03-04 10:43:16 +0100

    Update NewtonUniversalGravitation.java

--- src/simulator/model/NewtonUniversalGravitation.java
+++ src/simulator/model/NewtonUniversalGravitation.java
@@ -2,12 +2,32 @@ package simulator.model;
 
 import java.util.List;
 
+import simulator.misc.Vector2D;
+
 public class NewtonUniversalGravitation implements ForceLaws {
+double G;
+	
+	public NewtonUniversalGravitation(double g) {
+		super();
+		G = 6.67E-11;
+	}
+
 
 	@Override
 	public void apply(List<Body> bs) {
-		
-		
+		Vector2D fuerzaT = new Vector2D(0,0);
+		for (Body body : bs) {
+			fuerzaT.plus( body.getForce());
+			//TODO: sacar cuerpo iesimo con geti
+			//otro for
+			//
+			
+			//for (Body bodies : bs) {
+				
+			//}
+			
+			
+		}
 	}
 	
 	
