0dbdf7e783aa0500d1033890400e8d8c7ba961a4
crisselene <criisalways@gmail.com>
2021-03-11 10:10:25 +0100

    Update NewtonUniversalGravitation.java

--- src/simulator/model/NewtonUniversalGravitation.java
+++ src/simulator/model/NewtonUniversalGravitation.java
@@ -16,19 +16,45 @@ double G;
 	@Override
 	public void apply(List<Body> bs) {
 		Vector2D fuerzaT = new Vector2D(0,0);
-		for (Body body : bs) {
-			fuerzaT.plus( body.getForce());
-			//TODO: sacar cuerpo iesimo con geti
-			//otro for
-			//
-			
-			//for (Body bodies : bs) {
+		//obtenemos el primer cuerpo, iesimo
+		for (int i = 0; i < bs.size(); i++) {
+			Body iesimo = bs.get(i);
+			//caso especial en el que la masa del cuerpo es 0
+			if(iesimo.m == 0.0) {
+				iesimo.v.scale(0); //velocidad a 0
+				//la aceleración a 0 se hace en el move() de Body
 				
-			//}
+			}else {
+			//obtenemos segundo cuerpo, jesimo para aplicarle la ley a iesimo
+			for (int j = 0; j < bs.size(); j++) {
+				Body jesimo = bs.get(j);
+				if(jesimo!=iesimo) {
+					
+					//LEY NEWTON fij
+					//numerador ley Newton:
+					double numerador = (iesimo.m * jesimo.m);
+					//denominador ley Newton (ditancia al cuadrado):
+					double denominador = jesimo.p.distanceTo(iesimo.p);
+					Math.pow(denominador, 2);
+					//multiplicar por G para obtener fij
+					double fij = G * (numerador/denominador);
+					
+					//DIRECCION Fij
+					Vector2D dij = jesimo.p.minus(iesimo.p);
+					Vector2D Fij = dij.scale(fij);
+					
+					iesimo.f = Fij;
+					}
+				
+				}
+			}
+		}
+		
+		
+		
 			
 			
 		}
-	}
 	
 	
 }
