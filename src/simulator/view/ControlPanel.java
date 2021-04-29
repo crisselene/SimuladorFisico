949082a2bcb467493a48ddf0e23242c974e2ce62
crisselene <criisalways@gmail.com>
2021-04-29 20:43:16 +0200

    vista

--- src/simulator/view/ControlPanel.java
+++ src/simulator/view/ControlPanel.java
@@ -11,39 +11,93 @@ import javax.swing.JSpinner;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
+import java.awt.*;
 import java.awt.event.ActionListener;
 import simulator.control.Controller;
 import simulator.model.Body;
 import simulator.model.SimulatorObserver;
+import javax.swing.JLabel;
+
 
 public class ControlPanel extends JPanel implements SimulatorObserver {
 	// ...
 	private Controller _ctrl;
 	private boolean _stopped;
+	private JButton btnFileSelector;
+	private JButton btnRun;
+	private JButton btnPhysics;
+	private JButton btnStop;
+	private JTextField textField;
+	private JSpinner spinner;
+	private JLabel label;
+	private JLabel label_1;
+	private JLabel label_2;
+	private JLabel label_3;
+	private JLabel label_4;
+	private JLabel label_5;
+	private JLabel label_6;
 
 	ControlPanel(Controller ctrl) {
 		_ctrl = ctrl;
 		_stopped = true;
 		initGUI();
 		_ctrl.addObserver(this);
-	}
+		}
+	
+	
 
 	private void initGUI() {
-		JFileChooser fileChoose = new JFileChooser();
+		setSize(600, 200);//**** cambiar al ancho de la ventana la primer componente
+		setLayout(null);
+		setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		btnFileSelector = new JButton("");
+		btnFileSelector.setToolTipText("Select a File");
+		btnFileSelector.setIcon(new ImageIcon("C:\\Users\\Cris\\Desktop\\ARCHIVOS COMPLU\\workspace_tpII\\simuladorFisico\\resources\\icons\\open.png"));
+		add(btnFileSelector);
+		
+		
+		btnPhysics = new JButton("");
+		btnPhysics.setIcon(new ImageIcon("C:\\Users\\Cris\\Desktop\\ARCHIVOS COMPLU\\workspace_tpII\\simuladorFisico\\resources\\icons\\physics.png"));
+		add(btnPhysics);
+		
+		btnRun = new JButton("");
+		btnRun.setIcon(new ImageIcon("C:\\Users\\Cris\\Desktop\\ARCHIVOS COMPLU\\workspace_tpII\\simuladorFisico\\resources\\icons\\run.png"));
+		add(btnRun);
+		
+		btnStop = new JButton("");
+		btnStop.setIcon(new ImageIcon("C:\\Users\\Cris\\Desktop\\ARCHIVOS COMPLU\\workspace_tpII\\simuladorFisico\\resources\\icons\\stop.png"));
+		add(btnStop);
+		
+		JLabel lblNewLabel = new JLabel("Steps:");
+		add(lblNewLabel);
+		
+		spinner = new JSpinner();
+		add(spinner);
+		
+		JLabel lblNewLabel_1 = new JLabel("Delta-Time:");
+		add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		add(textField);
+		textField.setColumns(10);
+		
+		
+		/*JFileChooser fileChoose = new JFileChooser();
 		//botones
-		JButton buttonFile = this.createButton("botonLaws", 10, 10, 10, 10,"src/resources/icons/open.png","select a file");
+		JButton buttonFile = this.createButton( 10, 10, 10, 10,"../../src/resources/icons/open.png","select a file");
 		ActionListener FileButtonListener = new ActionListener() {
 			@Override
 			public void actionPerformed(java.awt.event.ActionEvent e) {
-				if(e.getSource() == this.load) {
+				//if(e.getSource() == this.load) {
 					
-				}
+				//}
 				
 			}
 		};
 		buttonFile.addActionListener(FileButtonListener);
 		
-		JButton botonLaws = this.createButton("botonLaws", 10, 10, 10, 10,"src/resources/icons/physics.png","select a force Law");
+		JButton botonLaws = this.createButton( 10, 20, 10, 10,"src/resources/icons/physics.png","select a force Law");
 		ActionListener LawsButtonListener = new ActionListener() {
 			@Override
 			public void actionPerformed(java.awt.event.ActionEvent e) {
@@ -53,7 +107,7 @@ public class ControlPanel extends JPanel implements SimulatorObserver {
 		};
 		botonLaws.addActionListener(LawsButtonListener);
 		
-		JButton resetButtons = this.createButton("resetButtons", 10, 10, 10, 10,"src/resources/icons/run.png","run and reset all the buttons");
+		JButton resetButtons = this.createButton( 10, 10, 10, 10,"src/resources/icons/run.png","run and reset all the buttons");
 		ActionListener resetButtonListener = new ActionListener() {
 			@Override
 			public void actionPerformed(java.awt.event.ActionEvent e) {
@@ -63,7 +117,7 @@ public class ControlPanel extends JPanel implements SimulatorObserver {
 		};
 		resetButtons.addActionListener(resetButtonListener);
 		
-		JButton stopButton = this.createButton("stop", 10, 10, 10, 10,"src/resources/icons/stop.png","stop the execution");
+		JButton stopButton = this.createButton( 10, 10, 10, 10,"src/resources/icons/stop.png","stop the execution");
 		ActionListener stopButtonListener = new ActionListener() {
 			@Override
 			public void actionPerformed(java.awt.event.ActionEvent e) {
@@ -73,7 +127,7 @@ public class ControlPanel extends JPanel implements SimulatorObserver {
 		};
 		stopButton.addActionListener(stopButtonListener);
 		
-		JButton exitButton = this.createButton("exit", 10, 10, 10, 10,"src/resources/icons/exit.png","exit");
+		JButton exitButton = this.createButton( 10, 10, 10, 10,"src/resources/icons/exit.png","exit");
 		ActionListener exitButtonListener = new ActionListener() {
 			@Override
 			public void actionPerformed(java.awt.event.ActionEvent e) {
@@ -88,13 +142,15 @@ public class ControlPanel extends JPanel implements SimulatorObserver {
 	}
 
 	
-	private JButton createButton(String caption, int x, int y, int w, int h, String path,String description){
-		JButton button = new JButton(caption);
-		button.setBounds(x,y,w,h);
-		button.setIcon(new ImageIcon(path));
+	private JButton createButton( int x, int y, int w, int h, String path,String description){
+		
+		JButton button = new JButton();
+		button.setBounds(208, 5, 33, 9);
+		button.setIcon(new ImageIcon("path"));
 		button.setToolTipText(description);
 		this.add(button);//***********?
 		return button;
+		*/
 	}
 	// other private/protected methods
 	// ...
@@ -157,5 +213,5 @@ public class ControlPanel extends JPanel implements SimulatorObserver {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
 }
